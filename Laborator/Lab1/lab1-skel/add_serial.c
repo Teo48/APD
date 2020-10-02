a33ead95a6cc164ad8a19aee6b6c7db5aa0a097a
Teo <teo_matei1999@yahoo.com>
2020-10-02 22:28:59 +0300

    Add Lab

---
+++ Laborator/Lab1/lab1-skel/add_serial.c
@@ -0,0 +1,95 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/*
+    schelet pentru exercitiul 5
+*/
+
+int* arr;
+int array_size;
+
+typedef struct Pair {
+    int start;
+    int end;
+} Pair;
+
+void* add(void *arg) {
+    Pair *p = (Pair *) arg;
+    
+    for (int i = p->start ; i < p->end ; ++i) {
+        arr[i] += 100;
+    }
+
+    free(p);
+    pthread_exit(NULL);
+}
+
+int min(int a, int b) { 
+    return a < b ? a : b;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        perror("Specificati dimensiunea array-ului\n");
+        exit(-1);
+    }
+
+    array_size = atoi(argv[1]);
+
+    arr = malloc(array_size * sizeof(*arr));
+
+    for (int i = 0; i < array_size; i++) {
+        arr[i] = i;
+    }
+
+    for (int i = 0; i < array_size; i++) {
+        printf("%d", arr[i]);
+        if (i != array_size - 1) {
+            printf(" ");
+        } else {
+            printf("\n");
+        }
+    }
+    
+    // TODO: aceasta operatie va fi paralelizata
+  	long cores = sysconf(_SC_NPROCESSORS_CONF);
+    pthread_t threads[cores];
+    int r;
+
+    for (int i = 0 ; i < cores ; ++i) {
+        Pair *p = malloc(sizeof(*p));
+        p->start = i * ((double) array_size / cores);
+        p->end = min((i + 1) * ((double) array_size / cores), array_size);
+        r = pthread_create(&threads[i], NULL, add, (void *)p);        
+        
+        if (r) {
+	  		    printf("Eroare la asteptarea thread-ului %d\n", i);
+	  		    exit(-1);
+		    }
+    }
+
+    /*
+       for (int i = 0; i < array_size; i++) {
+        arr[i] += 100;
+    }
+    */
+
+    for (int i = 0 ; i < cores ; ++i) {
+       r = pthread_join(threads[i], NULL);
+    }
+
+    for (int i = 0; i < array_size; i++) {
+        printf("%d", arr[i]);
+        if (i != array_size - 1) {
+            printf(" ");
+        } else {
+            printf("\n");
+        }
+    }
+    
+    free(arr);
+  	pthread_exit(NULL);
+
+    return 0;
+}
---
+++ Laborator/Lab1/lab1-skel/example.c
@@ -0,0 +1,78 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void *f(void *arg) {
+	int id = *(int* )arg;
+	
+	//for (int i = 0 ; i < 100 ; ++i) {
+	//	printf("Iteratia:%d Hello World din thread-ul %ld!\n", i, id);
+	//}
+    
+	printf("Hello World din thread-ul %d!\n", id);
+  	pthread_exit(NULL);
+}
+
+void *dummy_f(void *arg) {
+	int id = *(int *)arg;
+	
+    printf("Hello World din thread-ul %d!\n", id);
+  	
+    pthread_exit(NULL);
+}
+
+
+int main(int argc, char *argv[]) {
+    #define NUM_THREADS cores
+	long cores = sysconf(_SC_NPROCESSORS_CONF);
+	pthread_t threads[NUM_THREADS];
+	int r;
+    long id;
+    void *status;
+	
+  	/* Task 2-3
+       for (id = 0; id < NUM_THREADS; id++) {
+		r = pthread_create(&threads[id], NULL, f, (void *)id);
+
+		if (r) {
+	  		printf("Eroare la crearea thread-ului %ld\n", id);
+	  		exit(-1);
+		}
+  	}
+
+  	for (id = 0; id < NUM_THREADS; id++) {
+		r = pthread_join(threads[id], &status);
+
+		if (r) {
+	  		printf("Eroare la asteptarea thread-ului %ld\n", id);
+	  		exit(-1);
+		}
+  	}
+    */
+    
+    int thread_id[NUM_THREADS];
+    for (int i = 0 ; i < NUM_THREADS; ++i) {
+        thread_id[i] = i;
+    }
+
+    r = pthread_create(&threads[0], NULL, f, &thread_id[0]);
+	
+    if (r) {
+        printf("Eroare la crearea thread-ului 0\n");
+  		exit(-1);
+	}
+    
+    r = pthread_create(&threads[1], NULL, dummy_f, &thread_id[1]);
+	
+    if (r) {
+        printf("Eroare la crearea thread-ului 1\n");
+  		exit(-1);
+	}
+
+    pthread_join(threads[0], &status);
+    pthread_join(threads[1], &status);
+    pthread_exit(NULL);
+
+    return 0;
+}
